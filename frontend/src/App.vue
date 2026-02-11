4e092a85d2b0945a3cef4cf2ba5ea3ad6cda3b37
MagnusHSundby <Magnus.sundby2@gmail.com>
2026-02-11 08:55:31 +0100

    first test component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Gamecard from './components/Gamecard.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Gamecard :game="{ name: 'Chess', description: 'A classic strategy game' }"/>
 </template>
 
 <style scoped></style>
