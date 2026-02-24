9288313ef6b37b0c057b45224bce3be422484cc4
MagnusHSundby <Magnus.sundby2@gmail.com>
2026-02-24 14:34:46 +0100

    feat: add reusable ClickButton component

---
+++ frontend/src/components/ClickButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="custom-button">
+        {{ label }}
+    </button>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+    label: string;
+    color1?: string;
+    color2?: string;
+    animationDuration?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    label: 'Click Me',
+    color1:'#ffffff', 
+    color2:'#d0d0d0',
+    animationDuration: 5
+});
+
+</script>
