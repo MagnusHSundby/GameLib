6e666e39fc6f6a79969d5c352822a9641b2e5ea2
MagnusHSundby <Magnus.sundby2@gmail.com>
2026-03-11 11:26:32 +0100

    feat: add delete query to remove games

--- backend/src/routes/games.ts
+++ backend/src/routes/games.ts
@@ -1,26 +1,43 @@
-import { query } from '../dbconnector.js';  
-import express from 'express';
+import { release } from "os";
+import { query } from "../dbconnector.js";
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const games = await query('SELECT * FROM spill');
-        res.json(games);
-    } catch (error) {
-        console.error('Error fetching games:', error);
-        res.status(500).json({ error: 'Failed to fetch games' });
-    }
-});	
+router.get("/", async (req, res) => {
+  try {
+    const games = await query("SELECT * FROM spill");
+    res.json(games);
+  } catch (error) {
+    console.error("Error fetching games:", error);
+    res.status(500).json({ error: "Failed to fetch games" });
+  }
+});
 
-router.post('/', async (req, res) => {
-    const { name, genre, releaseDate } = req.body;
-    try {
-        const result = await query('INSERT INTO spill (navn, sjanger, utgivelsesdato) VALUES (?, ?, ?) RETURNING *', [name, genre, releaseDate]);
-        res.status(201).json(result[0]);
-    } catch (error) {
-        console.error('Error adding game:', error);
-        res.status(500).json({ error: 'Failed to add game' });
-    }
+router.post("/", async (req, res) => {
+  const { name, genre, releaseDate } = req.body;
+  try {
+    const result = await query(
+      "INSERT INTO spill (navn, sjanger, utgivelsesdato) VALUES (?, ?, ?) RETURNING *",
+      [name, genre, releaseDate],
+    );
+    res.status(201).json(result[0]);
+  } catch (error) {
+    console.error("Error adding game:", error);
+    res.status(500).json({ error: "Failed to add game" });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const result = await query("DELETE FROM spill WHERE navn = ? RETURNING *", [
+      name,
+    ]);
+    res.status(200).json(result[0]);
+  } catch (error) {
+    console.error("Error removing game:", error);
+    res.status(500).json({ error: "Failed to remove game" });
+  }
 });
 export default router;
